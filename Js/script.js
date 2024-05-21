2b3f338fda10d5e9dfeb16b94bf2da7b6d0e87ef
Lucas Diaz <lucasediaz75@gmail.com>
2024-05-21 08:43:00 -0300

    1er pre entrega

--- Js/script.js
+++ Js/script.js
@@ -1,14 +1,16 @@
-
+let confirmacion;
 let resultado;
-let vacio = " ";    
+let ingreseDireccion = "Pedido completado ingrese su direccion:";
+let metodoDePago = "Ingresa 1 si pagas con efectivo o 2 si pagas con trasferencia";
+let vacio = " ";
+const suma = (a, b, c) => a + b + c;
 
 function hacerSuma(art1, art2, art3) {
     resultado = art1 + art2 + art3;
-    return resultado;
 };
 
-let menu = parseInt(prompt("Menu de inicio: 1-Ingresa 1 si queres comprar comida 2-Ingresa 2 si queres comprar bebida"))
-
+let bienvenida = prompt("Ingrese su nombre")
+let menu = parseInt(prompt("Hola" + vacio + bienvenida + vacio + "este es el menu de inicio: 1-Ingresa 1 si queres comprar comida 2-Ingresa 2 si queres comprar bebida"))
 
 switch (menu) {
     case 1:
@@ -18,44 +20,48 @@ switch (menu) {
         let hamburguesa = parseInt(prompt("Cuantas hamburguesas queres comprar 1, 2 o 3?"))
         let nugget = parseInt(prompt("Cuantos nuggets queres comprar 1, 2 o 3?"))
 
+        confirmacion = prompt("Tu pedido total es de" + vacio + papas + vacio + "papas," + vacio + hamburguesa + vacio + "hamburguesas y" + vacio + nugget + vacio + "nuggets. Ingresa 1 si es correcto o 2 si es incorrecto");
 
-        hacerSuma(papas,hamburguesa,nugget)
-        alert("Tu pedido total es de" + vacio + resultado + vacio + "platos ");
-        
-        break;  
+        if (confirmacion == 1) {
+            alert("Buenisimo, ya casi terminamos")
+
+            direccion = prompt(ingreseDireccion);
+
+            prompt(metodoDePago)
+
+            let finalizacion = alert("Listo" + vacio + bienvenida + vacio + "tu pedido llegara pronto a:" + vacio + direccion + vacio + "muchas gracias por tu compra.")
+
+        }
+        else {
+            alert("ERROR! Intente nuevamente")
+        };
+
+        break;
     case 2:
-        alert("Bienvenido a la venta bebida")
+        alert("Bienvenido a la venta de bebidas")
 
         let agua = parseInt(prompt("Cuantas aguas queres comprar 1, 2 o 3?"));
         let gaseosa = parseInt(prompt("Cuantas gaseosas queres comprar 1, 2 o 3?"));
         let cerveza = parseInt(prompt("Cuantas cervezas queres comprar 1, 2 o 3?"));
 
-        hacerSuma(agua,gaseosa,cerveza)
-        alert("Tu pedido total es de" + vacio + resultado + vacio + "platos.");
+        confirmacion = prompt("Tu pedido total es de" + vacio + agua + vacio + "aguas," + vacio + gaseosa + vacio + "gaseosas y" + vacio + cerveza + vacio + "cerveza. Ingresa 1 si es correcto o 2 si es incorrecto")
 
+        if (confirmacion == 1) {
+            alert("Buenisimo, ya casi terminamos")
+            direccion = prompt(ingreseDireccion);
 
+            prompt(metodoDePago);
 
+            finalizacion = alert("Listo" + vacio + bienvenida + vacio + "tu pedido llegara pronto a:" + vacio + direccion + vacio + "muchas gracias por tu compra.");
 
-        let metodoDePago = prompt("Pagas con efectivo o transferencia?");
-
-        if (metodoDePago = "efectivo" || "transferencia"){
-            alert ("Buenisimo, ya casi terminamos")
-            let confirmacion = prompt ("Tu pedido total es de"+vacio+agua+vacio+"aguas,"+vacio+gaseosa+vacio+"gaseosas y"+vacio+cerveza+vacio+"cervezas. Confirmas que este correcto?? (Si o No)")
-            
-        }else {
+        }
+        else {
             alert("ERROR! Intente nuevamente")
         };
-
-        
-
-
-
 
         break;
     default:
         alert("ERROR! Intente nuevamente")
-        
         break;
 };
-
 
